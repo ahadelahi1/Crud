90ff3d2f01f3eb4dabb6d011c09fbe7c975f7889
AhadElahi <138647514+ahadelahi1@users.noreply.github.com>
2025-07-22 13:24:19 +0500

    new frn a

--- frontend/src/Comp/Register.jsx
+++ frontend/src/Comp/Register.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import "react-toastify/dist/ReactToastify.css"
 import { ToastContainer, toast } from 'react-toastify';
+
+
 export default function Register() {
 
   let [name, setName] = useState("");
@@ -9,12 +12,12 @@ export default function Register() {
   let [pswd, setPswd] = useState("");
   let [age, setAge] = useState(1);
 
+  const navigate = useNavigate();
+
   async function SubmitFun(){
     try {
-
       let u_r = /^(?!_)[a-zA-Z]{3,20}(?<!_)$/
       let p_r = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-
 
       if (!name || !email || !pswd || !age ){
         toast.error("all required")
@@ -39,45 +42,38 @@ export default function Register() {
       }).catch((e)=>{
         if(e.status === 409){
           toast.error(e.response.data.msg)
-          }
-          else{
+          } else {
             console.log(e)
-    
           }
         console.log(e.message)
       })
-    } 
-    
-    catch (error) {
-     
-    }
+    } catch (error) {}
   }
 
-
-
   return (
-
-<div>
+<div className="register-background">
  <ToastContainer/>
-<div class="form-container">
-  <h2>Register</h2>
+ <div className="form-container">
+    <h2>Register</h2>
 
-    <label for="name">Name*</label>
+    <label htmlFor="name">Name*</label>
     <input type="text" id="name" name="name" value={name} onChange={(e)=> setName(e.target.value)} required/>
 
-    <label for="email">Email*</label>
+    <label htmlFor="email">Email*</label>
     <input type="email" id="email" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
 
-    <label for="password">Password*</label>
+    <label htmlFor="password">Password*</label>
     <input type="password" id="password" name="password" value={pswd} onChange={(e)=> setPswd(e.target.value)} required/>
 
-    <label for="age">Age (optional)</label>
+    <label htmlFor="age">Age (optional)</label>
     <input type="number" id="age" name="age" value={age} onChange={(e)=> setAge(e.target.value)}/>
 
-    <button type="Button" class="submit-btn" onClick={SubmitFun}>Submit</button>
+    <button type="button" className="submit-btn" onClick={SubmitFun}>Submit</button>
 
+    <button type="button" className="go-showdata-btn" onClick={()=>navigate('/showdata')}>
+      Go to Show Data
+    </button>
+ </div>
 </div>
-
-    </div>
   )
 }
